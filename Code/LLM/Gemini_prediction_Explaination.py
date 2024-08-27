cad04757fafac2e34f980c702bc094e93a872323
Sneha <156975503+SnehaJ765@users.noreply.github.com>
2024-08-27 11:53:52 +0530

    Update Gemini_prediction_Explaination.py

--- Code/LLM/Gemini_prediction_Explaination.py
+++ Code/LLM/Gemini_prediction_Explaination.py
@@ -53,11 +53,6 @@ def get_text_chunks(data):
     return chunks
 
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
 
 def get_conversational_chain():
     prompt_template = """
